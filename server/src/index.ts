40df27ef1ae43252c442b939f5b30d03565c7580
Oleg Dramaretskyi <dramarec@gmail.com>
2021-04-17 08:43:06 +0300

    modularizing resolvers

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,10 @@
+import dotenv from "dotenv"
+dotenv.config();
+
 import express, { Application } from 'express';
 import { ApolloServer } from 'apollo-server-express';
 import { connectDatabase } from './database';
 import { typeDefs, resolvers } from './graphql';
-import dotenv from "dotenv"
-dotenv.config();
 
 const mount = async (app: Application) => {
     const db = await connectDatabase();
@@ -17,9 +18,6 @@ const mount = async (app: Application) => {
     app.listen(process.env.PORT);
 
     console.log(`[app] : http://localhost:${process.env.PORT}`);
-
-    const listings = await db.listings.find({}).toArray(); // listings is type any[]
-    console.log(listings);
 };
 
 mount(express());
