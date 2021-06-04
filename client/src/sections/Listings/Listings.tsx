278e7cb140e8e434ef4ec8166120868207b8b1e0
Oleg Dramaretskyi <dramarec@gmail.com>
2021-06-04 14:37:31 +0300

    Abstracting the type of data from server fetch

--- client/src/sections/Listings/Listings.tsx
+++ client/src/sections/Listings/Listings.tsx
@@ -1,5 +1,37 @@
 import React from "react";
+import { server } from "../../api";
+import { ListingsData } from "./types";
 
-export const Listings = () => {
-  return <h2>TinyHouse Listings</h2>;
+const LISTINGS = `
+    query Listings {
+        listings {
+            id
+            title
+            image
+            address
+            price
+            numOfGuests
+            numOfBeds
+            numOfBaths
+            rating
+        }
+    }
+`;
+
+interface Props {
+    title: string;
+
+}
+export const Listings = ({ title }: Props) => {
+    const fetchListings = async () => {
+        const { data } = await server.fetch<ListingsData>({ query: LISTINGS });
+        console.log(data); // check the console to see the listings data from our GraphQL Request!
+    };
+
+    return (
+        <div>
+            <h2>{title}</h2>
+            <button onClick={fetchListings}>Query Listings!</button>
+        </div>
+    );
 };
