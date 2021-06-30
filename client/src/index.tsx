2ed03c87956dced7c9d04437683b87e35486059e
Oleg Dramaretskyi <dramarec@gmail.com>
2021-06-30 17:32:37 +0300

    Cleared client, like new

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -47,7 +47,7 @@ const cache = new InMemoryCache({
 })
 
 const client = new ApolloClient({
-    connectToDevTools: true,
+    // connectToDevTools: true,
     uri: "/api",
     cache,
 });
@@ -69,13 +69,10 @@ const App = () => {
 
 render(
     <ApolloProvider client={client}>
-        {/* <Listings title="TinyHouse Listings" /> */}
         <App />
     </ApolloProvider>,
     document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
