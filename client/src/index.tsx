0f145c7c8b2ead58bdb4ca8c3de853bf2d7fbf01
Oleg Dramaretskyi <dramarec@gmail.com>
2021-07-16 19:08:23 +0300

    client added Login page with mutation, callback onCompleted, logInLoading

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,8 +1,11 @@
+import React, { useState } from "react";
 import { render } from 'react-dom';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import { Layout } from 'antd'
 
-import { Home, Host, Listing, Listings, NotFound, User } from "./sections";
+import { Viewer } from "./lib/types";
+import { Home, Host, Listing, Listings, Login, NotFound, User } from "./sections";
 import reportWebVitals from './reportWebVitals';
 import "./styles/index.css";
 
@@ -12,17 +15,39 @@ const client = new ApolloClient({
     cache: new InMemoryCache()
 });
 
+const initialViewer: Viewer = {
+    id: null,
+    token: null,
+    avatar: null,
+    hasWallet: null,
+    didRequest: false
+};
+
 const App = () => {
+    const [viewer, setViewer] = useState<Viewer>(initialViewer);
+    console.log("🔥🚀 ===> App ===> viewer", viewer);
+    console.log(viewer); // check the console to see viewer details after logging in
+
     return (
         <Router>
-            <Switch>
-                <Route exact path="/" component={Home} />
-                <Route exact path="/host" component={Host} />
-                <Route exact path="/listing/:id" component={Listing} />
-                <Route exact path="/listings/:location?" component={Listings} />
-                <Route exact path="/user/:id" component={User} />
-                <Route component={NotFound} />
-            </Switch>
+            <Layout id='app'>
+
+                <Switch>
+                    <Route exact path="/" component={Home} />
+                    <Route exact path="/host" component={Host} />
+                    <Route exact path="/listing/:id" component={Listing} />
+                    <Route exact path="/listings/:location?" component={Listings} />
+                    <Route
+                        exact
+                        path="/login"
+                        render={props => <Login {...props} setViewer={setViewer} />}
+                    />
+                    <Route exact path="/user/:id" component={User} />
+                    <Route component={NotFound} />
+
+                </Switch>
+
+            </Layout>
         </Router>
     );
 };
--- client/src/sections/Login/index.tsx
+++ client/src/sections/Login/index.tsx
@@ -1,13 +1,69 @@
-import React from "react";
-import { Card, Layout, Typography } from "antd";
+import React, { useEffect, useRef } from "react";
+import { Card, Layout, Spin, Typography } from "antd";
+import { useApolloClient, useMutation } from "@apollo/client";
+
+import { Viewer } from "../../lib/types"
+import { LOG_IN } from "../../lib/graphql/mutations"
+import { LogIn as LogInData, LogInVariables } from "../../lib/graphql/mutations/LogIn/__generated__/LogIn";
+import { AUTH_URL } from "../../lib/graphql/queries";
+import { AuthUrl as AuthUrlData } from "../../lib/graphql/queries/AuthUrl/__generated__/AuthUrl";
 
 // Image Assets
 import googleLogo from "./assets/google_logo.jpg";
 
+interface Props {
+    setViewer: (viewer: Viewer) => void;
+}
+
 const { Content } = Layout;
 const { Text, Title } = Typography;
 
-export const Login = () => {
+export const Login = ({ setViewer }: Props) => {
+    const client = useApolloClient();
+    const [
+        logIn,
+        { data: logInData, loading: logInLoading, error: logInError }
+    ] = useMutation<LogInData, LogInVariables>(LOG_IN, {
+        onCompleted: data => {
+            if (data && data.logIn) {
+                setViewer(data.logIn);
+            }
+        }
+    });
+
+    const logInRef = useRef(logIn);
+
+    useEffect(() => {
+        const code = new URL(window.location.href).searchParams.get("code");
+        if (code) {
+            logInRef.current({
+                variables: {
+                    input: { code }
+                }
+            });
+        }
+    }, []);
+
+    const handleAuthorize = async () => {
+        try {
+            const { data } = await client.query<AuthUrlData>({
+                query: AUTH_URL
+            })
+            window.location.href = data.authUrl;
+
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    if (logInLoading) {
+        return (
+            <Content className="log-in">
+                <Spin size="large" tip="Loggig you in..." />
+            </Content>
+        )
+    }
+
     return (
         <Content className="log-in">
             <Card className="log-in-card">
@@ -22,7 +78,7 @@ export const Login = () => {
                     </Title>
                     <Text>Sign in with Google to start booking available rentals!</Text>
                 </div>
-                <button className="log-in-card__google-button">
+                <button className="log-in-card__google-button" onClick={handleAuthorize} >
                     <img
                         src={googleLogo}
                         alt="Google Logo"
@@ -38,3 +94,22 @@ export const Login = () => {
         </Content>
     );
 };
+
+
+
+
+
+
+
+
+
+// - 
+// - The [`<Layout />`](https://ant.design/components/layout/) component helps handle the overall layout of a page.
+// - The [`<Card />`](https://ant.design/components/card/) component is a simple rectangular container (i.e. a card).
+// - The [`<Typography />`](https://ant.design/components/typography/) component gives us some simple styles for headings, body texts, and lists.
+// - onCompleted callback. https://www.apollographql.com/docs/tutorial/mutations/#support-user-login
+//  [`Spin`](https://ant.design/components/spin/) 
+// [`<Notification />`](https://ant.design/components/notification/)
+// [`<Message />`](https://ant.design/components/message)
+// [`<Alert />`](https://ant.design/components/alert/)
+//
