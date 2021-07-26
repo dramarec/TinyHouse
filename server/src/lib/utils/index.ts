f24159545eb367cb9af84df0cf11845b564abf79
Oleg Dramaretskyi <dramarec@gmail.com>
2021-07-26 21:12:11 +0300

    Server Building the Listing Resolvers

--- server/src/lib/utils/index.ts
+++ server/src/lib/utils/index.ts
@@ -1,16 +1,16 @@
 import { Request } from "express";
 import { Database, User } from "../types";
 
-export const authorize =
-    async (
-        db: Database,
-        req: Request
-    ): Promise<User | null> => {
-        const token = req.get("X-CSRF-TOKEN");
-        const viewer = await db.users.findOne({
-            _id: req.signedCookies.viewer,
-            token
-        });
+export const authorize = async (
+    db: Database,
+    req: Request
+): Promise<User | null> => {
+    const token = req.get("X-CSRF-TOKEN");
 
-        return viewer;
-    };
+    const viewer = await db.users.findOne({
+        _id: req.signedCookies.viewer,
+        token
+    });
+
+    return viewer;
+};
