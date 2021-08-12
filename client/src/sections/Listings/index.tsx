bd888fb9294baf852e7ce0b3274907703b243e96
Oleg Dramaretskyi <dramarec@gmail.com>
2021-08-12 22:55:21 +0300

    Filtering in the Listings page

--- client/src/sections/Listings/index.tsx
+++ client/src/sections/Listings/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { useQuery } from '@apollo/client';
 import { Link, RouteComponentProps } from "react-router-dom";
 import { Layout, List, Typography } from 'antd';
@@ -8,19 +9,23 @@ import {
     ListingsVariables
 } from '../../lib/graphql/queries/Listings/__generated__/Listings';
 import { ListingsFilter } from "../../lib/graphql/globalTypes";
+import { ListingsFilters } from './components/ListingsFilters';
+
 
 interface MatchParams {
     location: string;
 }
+
 const { Paragraph, Text, Title } = Typography;
 const { Content } = Layout;
 const PAGE_LIMIT = 8;
 
 export const Listings = ({ match }: RouteComponentProps<MatchParams>) => {
+    const [filter, setFilter] = useState(ListingsFilter.PRICE_LOW_TO_HIGH);
     const { data } = useQuery<ListingsData, ListingsVariables>(LISTINGS, {
         variables: {
             location: match.params.location,
-            filter: ListingsFilter.PRICE_LOW_TO_HIGH,
+            filter,
             limit: PAGE_LIMIT,
             page: 1
         }
@@ -29,21 +34,24 @@ export const Listings = ({ match }: RouteComponentProps<MatchParams>) => {
     const listings = data ? data.listings : null;
     const listingsRegion = listings ? listings.region : null;
 
-    const listingsSectionElement = listings && listings.result.length ? (
-        <List
-            grid={{
-                gutter: 8,
-                xs: 1,
-                sm: 2,
-                lg: 4
-            }}
-            dataSource={listings.result}
-            renderItem={listing => (
-                <List.Item>
-                    <ListingCard listing={listing} />
-                </List.Item>
-            )}
-        />
+    const listingsSectionElement = listings?.result.length ? (
+        <div>
+            <ListingsFilters filter={filter} setFilter={setFilter} />
+            <List
+                grid={{
+                    gutter: 8,
+                    xs: 1,
+                    sm: 2,
+                    lg: 4
+                }}
+                dataSource={listings.result}
+                renderItem={listing => (
+                    <List.Item>
+                        <ListingCard listing={listing} />
+                    </List.Item>
+                )}
+            />
+        </div>
     ) : (
         <div>
             <Paragraph>
