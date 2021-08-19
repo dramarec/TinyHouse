af1b3759ac3a2a7efb53658d72869bb51fa63d12
Oleg Dramaretskyi <dramarec@gmail.com>
2021-08-19 12:57:40 +0300

    Image uploads with Cloudinary

--- client/src/sections/Host/dist/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.Host = void 0;
-var react_1 = require("react");
-exports.Host = function () {
-    return react_1["default"].createElement("h2", null, "Host");
-};
