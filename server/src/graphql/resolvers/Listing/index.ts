abc1c87d3726ee2abe5c356c99434b812e3b5c1b
Oleg Dramaretskyi <dramarec@gmail.com>
2021-08-19 12:57:57 +0300

    Image uploads with Cloudinary

--- server/src/graphql/resolvers/Listing/index.ts
+++ server/src/graphql/resolvers/Listing/index.ts
@@ -1,7 +1,7 @@
 import { IResolvers } from "apollo-server-express";
 import { Request } from "express";
 import { ObjectId } from "mongodb";
-import { Google } from "../../../lib/api";
+import { Cloudinary, Google } from "../../../lib/api";
 import { Database, Listing, ListingType, User } from "../../../lib/types";
 import { authorize } from "../../../lib/utils";
 import {
@@ -126,9 +126,12 @@ export const listingResolvers: IResolvers = {
                 throw new Error("invalid address input");
             }
 
+            const imageUrl = await Cloudinary.upload(input.image);
+
             const insertResult = await db.listings.insertOne({
                 _id: new ObjectId(),
                 ...input,
+                image: imageUrl,
                 bookings: [],
                 bookingsIndex: {},
                 country,
